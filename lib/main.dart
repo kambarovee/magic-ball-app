0f53626a027a306e3d2ab1b48cb7db3eb0761ad1
Yerkebulan Kambarov <44515882+kambarovee@users.noreply.github.com>
2021-01-20 21:27:06 +0600

    End

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   runApp(MaterialApp(
@@ -19,8 +21,28 @@ class MagicBallPage extends StatefulWidget {
 }
 
 class _MagicBallPageState extends State<MagicBallPage> {
+  int changeBall = 1;
+
+  void randomizeBall() {
+    setState(() {
+      changeBall = Random().nextInt(5) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+              child: FlatButton(
+                onPressed: () {
+                  randomizeBall();
+                },
+                child: Image.asset('images/ball$changeBall.png'),
+              ))
+        ],
+      ),
+    );
   }
 }
